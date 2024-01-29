e4d29acf522879fca309b45f2c0a8646cb0d05e9
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2024-01-29 14:39:52 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ejercicio2Propiedades{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DateTime fechaHoy = DateTime.Today;
+
+            Console.WriteLine("DIA");
+            Console.WriteLine(fechaHoy.Day);
+            
+            Console.WriteLine(" ");
+            Console.WriteLine("MES");
+            Console.WriteLine(fechaHoy.Month);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("AÑO");
+            Console.WriteLine(fechaHoy.Year);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("HORA");
+            Console.WriteLine(fechaHoy);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("MINUTOS");
+            Console.WriteLine(fechaHoy.Minute);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("SEGUNDOS");
+            Console.WriteLine(fechaHoy.Second);
+   
+            Console.WriteLine(" ");
+            Console.WriteLine("DIA DE LA SEMANA");
+            Console.WriteLine(fechaHoy.DayOfWeek);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("EL DIA DEL AÑO");
+            Console.WriteLine(fechaHoy.DayOfYear);
+
+
+        }
+    }
+}
